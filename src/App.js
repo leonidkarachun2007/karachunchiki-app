b844d5bd1396a100390ace80d4f71204931e9aa5
undrcovver <tenetkoyehor@gmail.com>
2026-04-17 14:54:30 +0300

    second commit yehor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import Home from "./pages/Home";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          шо то леня даун
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Steam</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/data/games.js
@@ -0,0 +1,16 @@
+const games = [
+  {
+    id: 1,
+    title: "Cyberpunk 2077",
+    price: 29.99,
+    image: "https://via.placeholder.com/200"
+  },
+  {
+    id: 2,
+    title: "GTA V",
+    price: 19.99,
+    image: "https://via.placeholder.com/200"
+  }
+];
+
+export default games;
