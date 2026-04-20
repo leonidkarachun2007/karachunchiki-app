1b83bf78deeed00bba72fee83401b89567c68425
undrcovver <tenetkoyehor@gmail.com>
2026-04-20 19:44:34 +0300

    mainPage first commit

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,32 @@
 import './MainPage.css';
 
-const MainPage = () => (
-  <div className="MainPage">
-    
-  </div>
-);
+function MainPage () {
+  return (
+    <div className='main'>
+      <div className='hero'>
+
+        <div className='heroLeft'>
+          <img className='ApexImgLeft' src='/apexBack.jpeg' alt='apexLeft'/>
+        </div>
+
+        <div className='heroRight'>
+          <h2>APEX LEGENDS</h2>
+          <p>NEW SEASON</p>
+
+          <img className='ApexImgRight' src='/apexRightBackBottom.jpeg' alt='apexLeft'/>
+          <img className='ApexImgRight' src='/apexRightBack.jpeg' alt='apexLeft'/>
+
+          <div className='heroBottom'>
+            <span>FREE</span>
+            <button>Learn more</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
 
 export default MainPage;
