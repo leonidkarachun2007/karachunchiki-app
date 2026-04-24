575f75ae3da2999eeae176b6b163124ae210f39e
Leonid <Leonid@gmail.com>
2026-04-24 19:49:39 +0300

    add support without adapt

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import MainPage from './components/MainPage/MainPage';
 import Myheader from './components/Myheader/Myheader';
 import ProfilePage from "./components/ProfilePage/ProfilePage";
+import Support from './components/Support/Support';
 
 function App() {
   return (
     <div className="App">
       <Myheader/>
-      <h1>Проверка</h1>
       {/* <MainPage/> */}
-      {/* <ProfilePage/> */}
+      <ProfilePage/>
+      {/* <Support/> */}
     </div>
   );
 }
